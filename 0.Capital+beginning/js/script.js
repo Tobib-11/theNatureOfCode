e9bf2211abe95043e14d621a698028e42587c234
Tobib-11 <tobias.beran@student.htldornbirn.at>
2026-05-20 18:45:22 +0200

    test3

--- 0.Capital+beginning/js/script.js
+++ 0.Capital+beginning/js/script.js
@@ -1,12 +1,11 @@
 /** This is a setup function. */
 function setup() {
-  createCanvas(640, 240);
-  background(1);
-  circle(width / 2, height / 2, 200);
+  createCanvas(640, 440);
+  background(237, 232, 208);
+ 
 }
 
 /** This is a draw function. */
 function draw() {
-  rechtMode(CENTER);
-  square(random(width), random(height), 20);
+  
 }
